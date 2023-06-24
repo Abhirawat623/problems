88cba0d2495228eab07d3d0cbba2c03b26fbb009
Abhirawat623 <131130116+Abhirawat623@users.noreply.github.com>
2023-06-24 12:11:20 +0530

    Update async.js

--- async.js
+++ async.js
@@ -4,4 +4,6 @@ setTimeout(function(){
    console.log("wait here");},4000);
 
 console.log("be be be bee");
+//async function gives return (promise) while await waits for the promise to be settled
 
+            //we can use await for const and function too but async used before function instead of res=> res("resolved");
