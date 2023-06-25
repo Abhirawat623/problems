88940027b958aec23679a981c4c314a0867e4716
Abhirawat623 <131130116+Abhirawat623@users.noreply.github.com>
2023-06-25 14:21:59 +0530

    Update upper.js

--- upper.js
+++ upper.js
@@ -2,3 +2,4 @@ const string =" i m sakkari and i m not happy";
 console.log(string.toUpperCase());
 console.log(string);
 console.log(string.toLowerCase());
+    //after lowercase or uppercase. () needed to write
